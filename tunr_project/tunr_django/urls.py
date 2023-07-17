79eff5d380e9bd6e030f4873c5af8019e5afb575
ssstumbles <sara.rosic@gmail.com>
2023-07-17 13:42:03 -0400

    tunr

--- tunr_project/tunr_django/urls.py
+++ tunr_project/tunr_django/urls.py
@@ -1,7 +1,8 @@
-"""tunr_django URL Configuration
+"""
+URL configuration for tunr_django project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,10 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tunr.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework'))
 ]
